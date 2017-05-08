b5e54e35f955f38dede128551cfcbaec47007e25
jelliotartz <jelliotartz@gmail.com>
2017-05-08 09:58:41 -0500

    Update S3 bucket.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-DEST="s3://zooniverse-static/maintenance" # No trailing slash
+DEST="s3://zooniverse-static/whaling.oldweather.org" # No trailing slash
 PUBLISH_FILES=("index.html" "main.css" "img/shape.svg" "img/zooniverse-logo.svg")
 
 for FILE in ${PUBLISH_FILES[*]}
